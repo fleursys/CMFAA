1910a851c54feed659549df753804b595230fa2d
fleursys <fleursys@gmail.com>
2018-10-29 11:06:47 +0100

    Update UpwindSolver.py

--- UpwindSolver.py
+++ UpwindSolver.py
@@ -1,16 +1,21 @@
 import numpy as np
+from timeit import default_timer as timer
+
+start = timer()
+
 # variables to be changed
 nx = 10
-ngc = 2
-iniCond = 'acoustic'
-# iniCond = 'shock'
-# bc = 'fixed'
-bc = 'periodic'
+ngc = 1
+#iniCond = 'acoustic'
+iniCond = 'shock'
+bc = 'fixed'
+#bc = 'periodic'
 
-#variables that are fixed
+# variables that are fixed
 gamma = 1.4
+nw = 3
 
-#conservative variables in an array w with dimensions 3,1
+# conservative variables in an array w with dimensions 3,1
 def rho(w):
     return w[0]
 
@@ -18,13 +23,13 @@ def rhov(w):
     return w[1]
 
 def e(w):
-    return w[2]
+    return w[-1]
 
 def v(w):
     return rhov(w)/rho(w)
 
 def p(w):
-    return (gamma-1)*(-rhov(w)*rhov(w))/(2*rho(w))+ e(w)
+    return (gamma-1)*(-rhov(w)*rhov(w))/(2*rho(w)) + e(w)
 
 
 # returns an array with zeros with nx cells plus ngc ghostcells on each end
@@ -40,18 +45,22 @@ def initialization(x):
     if iniCond == 'shock':
         xmin = -0.5
         xmax = 0.5
-        for i in range(ngc, ngc + nx//2):
+        tmax = 0.2
+        dx = (xmax-xmin)/nx
+        for i in range(ngc, ngc + nx//2 + 1):
             x[i] = [8, 0, 8/gamma]
         for i in range(ngc + nx//2 + 1, nx + ngc):
             x[i] = [1, 0, 1]
-        return xmin, xmax, x
+        return tmax, dx, x
     elif iniCond == 'acoustic':
         xmin = 0
         xmax = 1
+        tmax = 3
+        dx = (xmax - xmin) / nx
         for i in range(ngc, ngc + nx):
             AcouP = 0.1 + 0.0001*np.exp(-((xmin + (xmax/(nx-1))*(i-2) - 0.5)*(xmin + (xmax/(nx-1))*(i-2) - 0.5))/0.01)
             x[i] = [AcouP*gamma, 0, AcouP]
-        return xmin, xmax, x
+        return tmax, dx, x
     else:
         print('no valid initial condition')
     return
@@ -62,7 +71,7 @@ def getbc(x):
     if bc == 'fixed':
         for i in range(ngc):
             x[i] = [8, 0, 8/gamma]
-            x[i-1] = [1, 0, 1]
+            x[-(i+1)] = [1, 0, 1]
         return x
     elif bc == 'periodic':
         for i in range(ngc):
@@ -72,23 +81,110 @@ def getbc(x):
     else:
         print('no valid boundary condition')
 
-# gives the eigenvalues
+# gives the eigenvalues as a 3 by nx+2*ngc matrix lam
 def getEigen(x):
     lam = []
-    for i in range(len(x)):
+    vlam = []
+    clam = []
+    for i in range(nx + 2*ngc):
         Cs = np.sqrt((gamma*p(x[i])/rho(x[i])))
         lam.append([v(x[i]) - Cs, v(x[i]), v(x[i]) + Cs])
-    return lam
+        vlam.append(v(x[i]))
+        clam.append(Cs)
+    return lam, vlam, clam
 
+# gives the eigenvector matrix for each set of  3 eigenvalues in lam
 def getK(lam):
+    K = []
+    for i in range(nx + 2*ngc):
+        K.append([])
+        for j in range(nw):
+            K[i].append([1, lam[i][j], (lam[i][j]*lam[i][j])/2])
+    return K
 
-def getKinv(lam):
+def getKinv(vlam, clam):
+    Kinv = []
+    for i in range(nx + 2*ngc):
+        Kinv.append([0,0,0])
+        Kinv[i][0] = [vlam[i]/(2*clam[i]), -1/2*clam[i]-vlam[i]/(clam[i]*clam[i]), 1/(clam[i]*clam[i])]
+        Kinv[i][1] = [1 - vlam[i]*vlam[i]/(clam[i]*clam[i]), 2*vlam[i]/(clam[i]*clam[i]), -2/(clam[i]*clam[i])]
+        Kinv[i][2] = [vlam[i]*vlam[i]/(2*clam[i]), -vlam[i]/(clam[i]*clam[i]) + 1/(2*clam[i]), 1/(clam[i]*clam[i])]
+    return Kinv
 
-def IntegrateTime():
-    mesh = MakeMesh()
-    xmin, xmax, mesh = initialization(mesh)
-    mesh = getbc(mesh)
-    eigenval = getEigen(mesh)
-    return eigenval
+def ConsToDiag(Kinv, x):
+    for i in range(nx + 2*ngc):
+        w = [0]*nw
+        for j in range(nw):
+            for k in range(nw):
+                w[j] = w[j] + Kinv[i][j][k]*x[i][k]
+        x[i] = w
+    return x
 
-print(IntegrateTime())
+def DiagtoCons(K, x):
+    for i in range(nx + 2*ngc):
+        w = [0]*nw
+        for j in range(nw):
+            for k in range(nw):
+                w[j] = w[j] + K[i][j][k]*x[i][k]
+        x[i] = w
+    return x
+
+def getFlux(lam, x, dx, dt):
+    f = [0]*(nx + 2*ngc)
+    for l in range(ngc):
+        f[l] = [0]*nw
+        f[-(l+1)] = [0]*nw
+    for i in range(nx):
+        f[i+1] = [0]*nw
+        for j in range(nw):
+            if lam[i+1][j] > 0:
+                f[i+1][j] = x[i+1][j] - dt*lam[i+j][j]*((x[i+1][j]-x[i][j])/dx)
+            else:
+                f[i + 1][j] = x[i + 1][j] - dt * lam[i + j][j] * ((x[i + 2][j] - x[i+1][j]) / dx)
+    return f
+
+def lamMax(lam):
+    lmax = 0
+    for i in range(nx + 2*ngc):
+        test = np.abs(lam[i][2])
+        if test > lmax:
+            lmax = test
+        else:
+            lmax = lmax
+    return lmax
+
+def computeTimeStep(lam,dx):
+    lmax = lamMax(lam)
+    return dx/lmax
+
+def IntegrateTime(x,dx):
+    lam, vlam, clam = getEigen(x)
+    dt = computeTimeStep(lam,dx)
+    K = getK(lam)
+    Kinv = getKinv(vlam,clam)
+    x = ConsToDiag(Kinv, x)
+    x = getFlux(lam,x,dx,dt)
+    x = DiagtoCons(K, x)
+    x = getbc(x)
+    return dt, x
+
+def UpwindSolver():
+    x = MakeMesh()
+    tmax, dx, x = initialization(x)
+    x = getbc(x)
+    t = 0
+    while t < tmax:
+        dt, x = IntegrateTime(x,dx)
+        t += dt
+    return
+
+UpwindSolver()
+
+end = timer()
+
+def chrono(start, end):
+    time = end-start
+    return time
+
+print("time elapsed = ")
+print(chrono(start,end))
