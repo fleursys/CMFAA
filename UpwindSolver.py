8491a12b59ec29e4c04def60be9c1436624fe4b6
fleursys <fleursys@gmail.com>
2018-10-29 18:24:45 +0100

    Update UpwindSolver.py

--- UpwindSolver.py
+++ UpwindSolver.py
@@ -1,18 +1,34 @@
 from timeit import default_timer as timer
-from TimeIntegration import initialization, MakeMesh, getbc, IntegrateTime
+from TimeIntegration import initialization, getbc, IntegrateTime
+from InputVariables import nx, ngc
+import matplotlib.pyplot as plt
 
 start = timer()
 
+def U(x,a):
+    u = []
+    for i in range(nx):
+        u.append(x[i+1][a])
+    return u
+
+def MakeMesh(xmin,xmax,dx):
+    g = [xmin]
+    for i in range(nx-2):
+        g.append(g[i]+dx)
+    g.append(xmax)
+    return g
+
 def UpwindSolver():
-    x = MakeMesh()
-    tmax, dx, x = initialization(x)
-    x = getbc(x)
-    print(x)
-    #t = 0
-    #while t < tmax:
-    #    dt, x = IntegrateTime(x,dx)
-    #    t += dt
-    return
+    tmax, xmax, xmin, dx, x = initialization()
+    t = 0
+    while t < tmax:
+        dt, x = IntegrateTime(x, dx)
+        u = U(x, 0)
+        X = MakeMesh(xmin, xmax, dx)
+        plt.plot(X, u, marker='.')
+        plt.show()
+        t += dt
+    return x, xmax, xmin, dx
 
 UpwindSolver()
 
@@ -24,3 +40,8 @@ def chrono(start, end):
 
 print("time elapsed = ")
 print(chrono(start,end))
+
+
+
+
+
