f0f01d6c101dab466a3942c14c8e365a48b12ec3
fleursys <fleursys@gmail.com>
2018-10-30 18:21:49 +0100

    Update FixedVariables.py

--- FixedVariables.py
+++ FixedVariables.py
@@ -1,8 +1,23 @@
 import numpy as np
+from InputVariables import iniCond, nx
 
 gamma = 1.4
 nw = 3
 nsnap = 30
+
+# determines the boundary conditions, xmin, xmax, dx and tmax from the initial condition
+if iniCond == 'acoustic':
+    bc = 'periodic'
+    xmin = 0
+    xmax = 1
+    dx = (xmax - xmin) / (nx - 1)
+    tmax = 3
+elif iniCond == 'shock':
+    bc = 'fixed'
+    xmin = -0.5
+    xmax = 0.5
+    dx = (xmax - xmin) / (nx - 1)
+    tmax = 0.2
 
 # computes the conservative variables from an array w of primitive variables (dimension nw)
 def rho(w):
@@ -15,7 +30,7 @@ def rhov(w):
 def e(w):
     return np.dot(w[1:-1],w[1:-1])*rho(w)/2 + w[-1]/(gamma-1)
 
-#computes the primitive variables from an array w of primitive variables (dimension nw)
+# computes the primitive variables and the speed of sound from an array w of primitive variables (dimension nw)
 
 def v(w):
     speed = [i/rho(w) for i in w[1:-1]]
@@ -23,3 +38,6 @@ def v(w):
 
 def p(w):
     return (gamma-1)*np.dot(w[1:-1], w[1:-1])/(2*rho(w)) + w[-1]
+
+def csound(w):
+    return np.sqrt((gamma*p(w)/rho(w)))
