e85173d48f7adade69527b349b780f00e3b21bac
fleursys <fleursys@gmail.com>
2018-11-01 17:38:40 +0100

    Update UpwindSolver.py

--- UpwindSolver.py
+++ UpwindSolver.py
@@ -1,19 +1,14 @@
-from timeit import default_timer as timer
 from TimeIntegration import initialization, IntegrateTime, make_mesh
-from InputVariables import nx, var, ngc, cvAnalysis
-from FixedVariables import xmin, xmax, dx, tmax, rho, p, v, nsnap
-from EOC import AnalyticShock, max_error
-import matplotlib.pyplot as plt
+from InputVariables import nx, var, ngc
+from FixedVariables import xmin, xmax, tmax, rho, p, v, nsnap
 
-start = timer()
-
-def U(x,var):
+def U(x,var, nx):
     u = []
     if var == 'density':
         for i in range(ngc, nx + ngc):
             u.append(rho(x[i]))
     elif var == 'velocity':
-        for i in range(nx):
+        for i in range(ngc, nx + ngc):
             u.append(v(x[i])[0])
     elif var == 'pressure':
         for i in range(ngc, ngc + nx):
@@ -22,43 +17,25 @@ def U(x,var):
         print('no valid variable')
     return u
 
-def UpwindSolver():
+def upwind_solver(nx):
     solution = []
-    x = initialization()
+    x = initialization(nx)
     t = 0
-    time_snap = tmax/(nsnap)
-    solution.append(U(x, var))
+    time_snap = tmax/nsnap
+    snap = time_snap
+    solution.append(U(x, var, nx))
     while t < tmax:
-        if t >= time_snap:
-            dt, x = IntegrateTime(x, dx)
+        if t >= snap:
+            dt, x = IntegrateTime(x, nx)
             t += dt
-            solution.append(U(x, var))
-            time_snap += time_snap
+            solution.append(U(x, var, nx))
+            snap += time_snap
         else:
-            dt, x = IntegrateTime(x, dx)
+            dt, x = IntegrateTime(x, nx)
             t += dt
-    solution.append(U(x, var))
+    solution.append(U(x, var, nx))
     return solution
 
-def cv_and_conquer():
-    num_sol = UpwindSolver()[-1]
-    a_sol = AnalyticShock()
-    err_1 = max_error(num_sol, a_sol)
-    err_2 = 3
-    return err_1 - err_2
 
 
-Solution = UpwindSolver()
-
-if cvAnalysis == 'true':
-    a = 2
-
-end = timer()
-
-def chrono(start, end):
-    time = end-start
-    return time
-
-print("time elapsed = ")
-print(chrono(start,end))
 
