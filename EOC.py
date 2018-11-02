070c4de087a36288e0a2ef8568e91e44cb5a5fb7
fleursys <fleursys@gmail.com>
2018-11-02 16:51:59 +0100

    Update EOC.py

--- EOC.py
+++ EOC.py
@@ -2,6 +2,7 @@ import numpy as np
 from UpwindSolver import upwind_solver
 from AnalyticSolution import analytic_solver
 
+# gives the maximum error between the numerical solution and the analytical solution on nx grid points
 def max_error(num_sol, a_sol, nx):
     max = 0
     for i in range(nx):
@@ -10,9 +11,11 @@ def max_error(num_sol, a_sol, nx):
             max = test
     return max
 
+# subroutine to determine the length of the grid based on nu
 def N(nu):
     return np.power(2, nu)
 
+# computes the empirical order of convergence for nu
 def EOC(nu):
     num1 = upwind_solver(N(nu-1))[-1]
     ana1 = analytic_solver(N(nu-1), 0.2)
@@ -23,9 +26,10 @@ def EOC(nu):
     eoc = np.log2(err2/err1) / np.log2(N(nu - 1)/N(nu))
     return eoc
 
-def cv_and_conquer():
+# gives an array of the empirical error of convergence from nu = 2 to nu_max
+def cv_and_conquer(nu_max):
     cv = []
-    for nu in range(2,10):
+    for nu in range(2, nu_max):
         cv.append(EOC(nu))
     return cv
 
